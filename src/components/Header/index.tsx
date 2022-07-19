7a721314d24b861c3088874703822bccbcf7ccd3
lyrarod <lyrarod@gmail.com>
2022-07-18 22:38:01 -0300

    add arrowLeft in Header component > continent page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,27 +1,58 @@
 import Link from "next/link";
 import { Flex, Image } from "@chakra-ui/react";
+import { ChevronLeftIcon } from "@chakra-ui/icons";
+
+import { useRouter } from "next/router";
 
 export const Header = () => {
+  const { asPath } = useRouter();
+
   return (
     <Flex
       as={"header"}
       w={"full"}
-      maxW={"1440px"}
+      maxW={"1160px"}
       h={"100px"}
       mx={"auto"}
       align={"center"}
       justify={"center"}
+      position={"relative"}
     >
-      <Link href={"/"}>
-        <a>
-          <Image
-            src={"/assets/logo.svg"}
-            alt="logo"
-            width={"184.06px"}
-            height={"45.92px"}
-          />
-        </a>
-      </Link>
+      {asPath !== "/" && (
+        <Link href={"/"}>
+          <a>
+            <ChevronLeftIcon
+              w={"2em"}
+              h={"2em"}
+              color="#47585B"
+              position={"absolute"}
+              left={0}
+              top={"50%"}
+              transform={"translateY(-50%)"}
+            />
+          </a>
+        </Link>
+      )}
+
+      {asPath === "/" ? (
+        <Image
+          src={"/assets/logo.svg"}
+          alt="logo"
+          width={"184.06px"}
+          height={"45.92px"}
+        />
+      ) : (
+        <Link href={"/"}>
+          <a>
+            <Image
+              src={"/assets/logo.svg"}
+              alt="logo"
+              width={"184.06px"}
+              height={"45.92px"}
+            />
+          </a>
+        </Link>
+      )}
     </Flex>
   );
 };
